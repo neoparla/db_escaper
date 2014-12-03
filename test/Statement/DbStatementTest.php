122c8af7ce39063e0edeec7e19a76a8493ac2942
Pau Perez <pau.perez@softonic.com>
2014-12-03 18:28:05 +0100

    DbStatement test. Not finished

---
+++ test/Statement/DbStatementTest.php
@@ -0,0 +1,140 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: pau.perez
+ * Date: 12/3/14
+ * Time: 5:15 PM
+ */
+
+namespace NeoParla\DbEscaper\Statement;
+
+
+use PHPUnit_Framework_MockObject_MockObject;
+use PHPUnit_Framework_TestCase;
+
+class DbStatementTest extends PHPUnit_Framework_TestCase {
+    const DEFAULT_QUERY = 'query';
+    const DEFAULT_LABEL = 'Label';
+    const DEFAULT_BINDING = 'String';
+
+    /**
+     * @var DbStatement
+     */
+    private $statement;
+
+    private function randomString() {
+        return substr(str_shuffle("0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"), 0, rand(1,1000));
+    }
+
+    /**
+     * @test
+     */
+    public function constructorParameters() {
+        $link = $this->getLinkMocked();
+        $query = $this->randomString();
+        $label = $this->randomString();
+
+        $obj = new DbStatement($link, $query, $label);
+
+        $this->assertAttributeSame($link, 'link', $obj);
+        $this->assertAttributeSame($query, 'query', $obj);
+        $this->assertAttributeSame($label, 'label', $obj);
+    }
+
+    /**
+     * @test
+     */
+    public function gettingLastError() {
+        $link = $this->getLinkMocked();
+        $mocked_value = rand(0, 100);
+        $link
+            ->expects($this->once())
+            ->method('getLastError')
+            ->will($this->returnValue($mocked_value))
+        ;
+
+        $obj = new DbStatement($link, self::DEFAULT_QUERY, self::DEFAULT_QUERY);
+
+        $this->assertSame($mocked_value, $obj->getError(), 'Getting last error should return the same as Link::getLastError');
+    }
+
+    /**
+     * @test
+     */
+    public function bindingParamWhenInvalidType() {
+
+        $this->setExpectedException(__NAMESPACE__ . '\\Binding\\BindingException', 'Invalid binding type "InvalidType"');
+
+        $obj = new DbStatement($this->getLinkMocked(), self::DEFAULT_QUERY, self::DEFAULT_LABEL);
+        $value = self::DEFAULT_QUERY;
+        $obj->bindParam(self::DEFAULT_QUERY, $value, 'InvalidType');
+    }
+
+    /**
+     * @test
+     */
+    public function bindingParameterWhenValidType() {
+        $obj = new DbStatement($this->getLinkMocked(), self::DEFAULT_QUERY, self::DEFAULT_LABEL);
+        $search = $this->randomString();
+        $value = 'value';
+
+        $returned = $obj->bindParam($search, $value, self::DEFAULT_BINDING);
+
+        $parameters = $this->readAttribute($obj, 'parameters');
+
+        $this->assertCount(1, $parameters, 'If only one bindParam called, only one parameter');
+        $this->assertEquals(array('search', 'binding'), array_keys($parameters[$search]), 'Each parameter MUST have both search and binding keys defined');
+        $this->assertEquals($search, $parameters[$search]['search'], 'Parameter search value must be as defined on method call');
+        $this->assertInstanceOf(__NAMESPACE__ . '\\Binding\\Binding', $parameters[$search]['binding']);
+        $this->assertSame($returned, $obj, 'Fluent interface required');
+    }
+
+    /**
+     * @test
+     */
+    public function bindingMultipleParameters() {
+        $obj = new DbStatement($this->getLinkMocked(), self::DEFAULT_QUERY, self::DEFAULT_LABEL);
+
+        $value = 'value';
+        // Forcing invalid bindings, will be overwritten.
+        $obj->bindParam('to_replace', $value, self::DEFAULT_BINDING);
+        $obj->bindParam('to_replace_2', $value, self::DEFAULT_BINDING);
+        $obj->bindParam('to_replace_3', $value, self::DEFAULT_BINDING);
+
+        $parameters = $this->readAttribute($obj, 'parameters');
+        $this->assertCount(3, $parameters, 'If more than one binding, more than one parameter');
+    }
+
+    /**
+     * @test
+     */
+    public function bindingMultipleTimesSameParameter() {
+        $obj = new DbStatement($this->getLinkMocked(), self::DEFAULT_QUERY, self::DEFAULT_LABEL);
+
+        $value = 'value';
+        // Forcing invalid bindings, will be overwritten.
+        $obj->bindParam('to_replace', $value, self::DEFAULT_BINDING);
+        $parameters = $this->readAttribute($obj, 'parameters');
+        $value_binded_first_call = $this->readAttribute($parameters['to_replace']['binding'], 'value');
+        $obj->bindParam('to_replace', $value, self::DEFAULT_BINDING);
+        $value = 'different_value';
+        $obj->bindParam('to_replace', $value, self::DEFAULT_BINDING);
+
+        $parameters = $this->readAttribute($obj, 'parameters');
+        $value_binded_last_call = $this->readAttribute($parameters['to_replace']['binding'], 'value');
+
+
+        $this->assertNotSame($value, $value_binded_first_call);
+        $this->assertSame($value, $value_binded_last_call);
+        $this->assertCount(1, $parameters, 'If more than one binding, more than one parameter');
+    }
+
+    /**
+     * @return PHPUnit_Framework_MockObject_MockObject
+     */
+    private function getLinkMocked()
+    {
+        $link = $this->getMockForAbstractClass('NeoParla\\DbEscaper\\Link');
+        return $link;
+    }
+} 
